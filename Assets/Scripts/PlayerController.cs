61ea8b77519307d11c3a200f0e73fd811c0aa869
Herick Chan <herick.chan@gmail.com>
2016-02-20 17:48:13 -0500

    Pilot

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody rb;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(Input.acceleration.x, 0.0f, Input.acceleration.y);
+        rb.velocity = movement * speed;
+    }
+}
