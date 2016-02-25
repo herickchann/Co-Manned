e3181c497b99fec7d5dd121c503c1151dff826b0
Herick Chan <herick.chan@gmail.com>
2016-02-24 22:33:53 -0500

    Changed control scheme to use onscreen joystick and touch to fire

--- Assets/Scripts/MechController.cs
+++ Assets/Scripts/MechController.cs
@@ -1,65 +1,95 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
 
 public class MechController : MonoBehaviour
 {
     public float speed;
     private Rigidbody rb;
-	private Quaternion _lookRotation;
-	private Vector3 _direction;
+    private RectTransform joystickTrans;
 
 	public GameObject bullet;
+    public GameObject joystick;
 	public Transform bulletSpawn;
 	private float nextFire;
 	public float fireRate;
 
-    void Start() {
+    private float moveH;
+    private float moveV;
+    //private bool joystickHidden = true;
+    //private Vector3 joystickLocation;
+
+    void Start () {
         rb = GetComponent<Rigidbody>();
+        //joystickTrans = joystick.GetComponent<RectTransform>();
+        //joystickLocation = joystickTrans.position;
     }
-		
-	void Update() {
-		foreach (Touch touch in Input.touches) {
-			RaycastHit hit;
-			Ray ray = Camera.main.ScreenPointToRay(touch.position);
 
-			if (Physics.Raycast (ray, out hit)) {
-				if (hit.transform.name == "GroundPlane") {
-					if (touch.phase == TouchPhase.Began || touch.phase == TouchPhase.Moved) {
-						transform.LookAt(hit.point);
-					}
-				}
-			}
-		}
+    void Update () {
+        //joystick.SetActive(joystickHidden);
+        //joystickTrans.position = joystickLocation;
 
-		if (Input.GetMouseButtonDown(0))
-		{
-			RaycastHit hit;
-			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-			if (Physics.Raycast(ray, out hit)) {
+        moveH = CrossPlatformInputManager.GetAxis("Horizontal");
+        moveV = CrossPlatformInputManager.GetAxis("Vertical");
 
-				Debug.Log (hit.transform.name);
-				if (hit.transform.name == "GroundPlane") {
-					transform.LookAt(hit.point);
-				}
-		
-			}
-		}
-
-		if (Input.GetButton ("Fire1") && Time.time > nextFire) {
-			nextFire = Time.time + fireRate;
-			GameObject clone = Instantiate (bullet, bulletSpawn.position, bulletSpawn.rotation) as GameObject;
-		}
-
-	}
-
-    void FixedUpdate() {
-//		float moveH = Input.GetAxis ("Horizontal");
-//		float moveV = Input.GetAxis ("Vertical");
-//
-//		Vector3 movement = new Vector3(moveH, 0.0f, moveV);
-
-		Vector3 movement = new Vector3(Input.acceleration.x, 0.0f, Input.acceleration.y);
-		//movement = transform.TransformDirection(movement);
-		rb.velocity = movement * speed;
+        Move();
+        Turn();
+        Fire();
+        //ToggleJoystick();
     }
+
+    private void Move () {
+        Vector3 movement = new Vector3(moveH, 0.0f, moveV);
+        //Vector3 movement = new Vector3(Input.acceleration.x, 0.0f, Input.acceleration.y);
+        //movement = transform.TransformDirection(movement);
+        rb.velocity = movement * speed;
+    }
+
+    private void Turn () {
+        float angle = Mathf.Atan2(moveH, moveV) * Mathf.Rad2Deg;
+        if (angle != -180) {
+            rb.rotation = Quaternion.Euler(new Vector3(0, angle, 0));
+        }
+    }
+
+    private void Fire () {
+        foreach (Touch touch in Input.touches) {
+            if (touch.phase == TouchPhase.Began) {
+                if (touch.position.x > (Screen.width / 2) && Time.time > nextFire) {
+                    nextFire = Time.time + fireRate;
+                    Instantiate(bullet, bulletSpawn.position, bulletSpawn.rotation);
+                }
+            }
+        }
+
+        if (Input.GetMouseButtonDown(0)) {
+            if (Input.mousePosition.x > (Screen.width / 2) && Time.time > nextFire) {
+                nextFire = Time.time + fireRate;
+                Instantiate (bullet, bulletSpawn.position, bulletSpawn.rotation);
+            }
+        }
+    }
+
+    //private void ToggleJoystick () {
+    //    foreach (Touch touch in Input.touches) {
+    //        if (touch.phase == TouchPhase.Began) {
+    //            if (touch.position.x < (Screen.width / 2)) {
+    //                joystickTrans.position = touch.position;
+    //            }
+    //        } 
+    //    }
+
+    //    if (Input.GetMouseButtonDown(0)) {
+    //        if (Input.mousePosition.x < (Screen.width / 2)) {
+    //            joystickLocation = Input.mousePosition;
+    //            joystickHidden = true;
+    //        }
+    //    }
+    //    if (Input.GetMouseButtonUp(0)) {
+    //        joystickHidden = true;
+    //    }
+
+    //    joystick.SetActive(joystickHidden);
+    //    joystickTrans.position = joystickLocation;
+    //}
 }
