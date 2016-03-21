8521f0b59f844f379838ee593f35a16a5e66d47e
Herick Chan <herick.chan@gmail.com>
2016-03-21 13:12:07 -0500

    Fixed spawning of powerups for multiplayer

--- Assets/Scripts/NetworkManagerScript.cs
+++ Assets/Scripts/NetworkManagerScript.cs
@@ -5,10 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class NetworkManagerScript : NetworkManager {
 
-    // keep track of spawn points
-    private Transform[] powerupSpawns;
-    private GameObject[] powerUps;
-
     // wire up game manager
     public GameObject gameManager;
 
@@ -33,9 +29,6 @@ public class NetworkManagerScript : NetworkManager {
         GameObject player = (GameObject)Instantiate(playerPrefab, Vector3.zero, Quaternion.identity);
         NetworkServer.AddPlayerForConnection(conn, player, playerControllerId);
 
-        if (numPlayers > 1) {
-            SpawnPowerups();
-        }
     }
 
     public override void OnServerConnect (NetworkConnection conn) {
@@ -58,25 +51,6 @@ public class NetworkManagerScript : NetworkManager {
             Debug.Log("entered as engineer");
             //ClientScene.RemovePlayer (0);
             SceneManager.LoadScene("engineer");
-        }
-    }
-    
-    private void SpawnPowerups() {
-        powerUps = Resources.LoadAll<GameObject>("Powerups");
-        Transform spawns = GameObject.Find("PowerupSpawnPoints").transform;
-        powerupSpawns = new Transform[spawns.childCount];
-        for (int i = 0; i < spawns.childCount; i++) {
-            powerupSpawns[i] = spawns.GetChild(i);
-        }
-
-        if (powerupSpawns != null) {
-            foreach (Transform powerupPoint in powerupSpawns) { 
-                //Randomize powerup types
-                int index = Random.Range(0, powerUps.Length);
-
-                GameObject powerUp = (GameObject)Instantiate(powerUps[index], powerupPoint.position, powerupPoint.rotation);
-                NetworkServer.Spawn(powerUp);
-            }
         }
     }
 }
