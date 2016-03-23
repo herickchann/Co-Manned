11726fe2b76d9601e0b27d3979067e13c381d2ef
Herick Chan <herick.chan@gmail.com>
2016-03-23 13:58:05 -0500

    Added new mech assets

---
+++ Assets/_DLNK/DLNK DemoScenes/Armored Golem/Scripts/buttonleft.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class buttonleft : MonoBehaviour 
+{
+	public Animator _animator;
+	public GUIText AnimText;
+
+//	// Use this for initialization
+//	void Start () {
+//	
+//	}
+//	
+//	// Update is called once per frame
+//	void Update () {
+//	
+//	}
+
+	public void OnMouseUp()
+	{
+		int tmp;
+		tmp = _animator.GetInteger("AnimNum");
+		tmp--;
+
+		if (tmp < 1)
+		{
+			tmp = 11;
+		}
+
+		_animator.SetInteger("AnimNum", tmp);
+
+		
+		if (tmp == 1){
+			AnimText.text = "Stand Idle";
+		}
+		if (tmp == 2){
+			AnimText.text = "Stand Lookaround";
+		}
+		if (tmp == 3){
+			AnimText.text = "Get Damage 1";
+		}
+		if (tmp == 4){
+			AnimText.text = "Get Damage 2";
+		}
+		if (tmp == 5){
+			AnimText.text = "Get Damage 3";
+		}
+		if (tmp == 6){
+			AnimText.text = "Combat Attack 1";
+		}
+		if (tmp == 7){
+			AnimText.text = "Combat Attack 2";
+		}
+		if (tmp == 8){
+			AnimText.text = "Combat Attack 3";
+		}
+		if (tmp == 9){
+			AnimText.text = "Walk in Position";
+		}
+		if (tmp == 10){
+			AnimText.text = "Simple Jump";
+		}
+		if (tmp == 11){
+			AnimText.text = "Fall Down / Die";
+		}
+
+		//DLNK ASSETS
+	}
+}
---
+++ Assets/_DLNK/Generic Resources/Scripts/ChangeRotate.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeRotate : MonoBehaviour {
+
+	public Rotate RotateObject;
+	public float RotationAdditive;
+	public bool Substract=false;
+
+	// Use this for initialization
+	void OnMouseUp () {
+
+		if (!Substract) {
+			RotateObject.RotationFactor=(RotateObject.RotationFactor)+RotationAdditive;
+		}
+		else
+			RotateObject.RotationFactor=(RotateObject.RotationFactor)-RotationAdditive;
+	}
+	
+	// Update is called once per frame
+	//DLNK ASSETS
+	void Update () {
+	
+	}
+}
