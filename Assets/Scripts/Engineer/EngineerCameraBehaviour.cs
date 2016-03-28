b84fb342f38f9bec374f9c8047561e005369c362
Adrian Smith <amsmith209@gmail.com>
2016-03-28 18:59:47 -0400

    Fixes bug in camera script which refers to Main camera instead of itself

--- Assets/Scripts/Engineer/EngineerCameraBehaviour.cs
+++ Assets/Scripts/Engineer/EngineerCameraBehaviour.cs
@@ -27,27 +27,27 @@ public class EngineerCameraBehaviour : MonoBehaviour
 			if (touch.phase == TouchPhase.Began)
 			//if (Input.GetMouseButtonDown(0))
 			{
-				//Destroy(Camera.main);
+				//Destroy(gameObject.GetComponent<Camera>());
 					RaycastHit hit;
-					//Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-					Ray ray = Camera.main.ScreenPointToRay(touch.position);
+					//Ray ray = gameObject.GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+					Ray ray = gameObject.GetComponent<Camera>().ScreenPointToRay(touch.position);
 					if (Physics.Raycast(ray, out hit))
 					{
 						if (hit.transform.gameObject.tag == "EnergyCell") { 
                         //Destroy(hit.transform.gameObject);
                         scr = hit.transform.gameObject.GetComponent<AmmoBehaviour>();
                         toDrag = hit.transform;
-                        dist = hit.transform.position.z - Camera.main.transform.position.z;
+                        dist = hit.transform.position.z - gameObject.GetComponent<Camera>().transform.position.z;
                         v3 = new Vector3(touch.position.x, touch.position.y, dist);
                         //v3 = new Vector3(Input.mousePosition.x, Input.mousePosition.y, dist);
-                        v3 = Camera.main.ScreenToWorldPoint(v3);
+                        v3 = gameObject.GetComponent<Camera>().ScreenToWorldPoint(v3);
                         //v3.z += 20;
                         //	offset = toDrag.position - v3;
                         //toDrag.position = offset;
                         dragging = true;
                         scr.SetSnapBack(false);
                         toDrag.gameObject.GetComponent<Rigidbody>().detectCollisions = false;
-                        //Destroy(Camera.main);
+                        //Destroy(gameObject.GetComponent<Camera>());
 					}
 					}
 				}
@@ -58,7 +58,7 @@ public class EngineerCameraBehaviour : MonoBehaviour
 					{
 					v3 = new Vector3(touch.position.x, touch.position.y, dist);
 					//v3 = new Vector3(Input.mousePosition.x, Input.mousePosition.y, dist);
-					v3 = Camera.main.ScreenToWorldPoint(v3);
+					v3 = gameObject.GetComponent<Camera>().ScreenToWorldPoint(v3);
 						toDrag.position = v3 + offset;
 					}
 				}
@@ -78,16 +78,16 @@ public class EngineerCameraBehaviour : MonoBehaviour
 			if (Input.GetMouseButtonDown(0))
 			{
 					RaycastHit hit;
-					Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+					Ray ray = gameObject.GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
 					if (Physics.Raycast(ray, out hit))
 					{
 						if (hit.transform.gameObject.tag == "EnergyCell")
 						{
 						scr= hit.transform.gameObject.GetComponent<AmmoBehaviour>();
 							toDrag = hit.transform;
-							dist = hit.transform.position.z - Camera.main.transform.position.z;
+							dist = hit.transform.position.z - gameObject.GetComponent<Camera>().transform.position.z;
 						v3 = new Vector3(Input.mousePosition.x, Input.mousePosition.y, dist);
-						v3 = Camera.main.ScreenToWorldPoint(v3);
+						v3 = gameObject.GetComponent<Camera>().ScreenToWorldPoint(v3);
 						v3.z += 20;
 							offset = toDrag.position - v3;
 						toDrag.position = offset;
@@ -101,7 +101,7 @@ public class EngineerCameraBehaviour : MonoBehaviour
 					if (dragging)
 					{
 					v3 = new Vector3(Input.mousePosition.x, Input.mousePosition.y, dist);
-					v3 = Camera.main.ScreenToWorldPoint(v3);
+					v3 = gameObject.GetComponent<Camera>().ScreenToWorldPoint(v3);
 						toDrag.position = v3 + offset;
 					}
 				}
